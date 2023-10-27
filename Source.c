d4c876c37d9f9f380773f4fcedd1f536ac87fe2f
Dmitry <andrewgamer63@gmail.com>
2023-10-27 20:19:29 +0300

    func overload

--- Source.c
+++ Source.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #define N 20
+//#include "./test.txt"
 
-
-void findMinMax(const float matrix[][N], int size_n, int size_m, float* max, float* min) {
-	*min = *max = matrix[0][0];
-	for (int i = 0; i < size_n; i++){
-		for (int j = 0; j < size_m; j++) {
-			float currEl = matrix[i][j];
-			if (*min > currEl) *min = currEl;
-			if (*max < currEl) *max = currEl;
-		}
+int mSizeInput(int *size) {
+	int size_n = 0;
+	if (!scanf_s("%d", &size_n) || size_n > 20 || size_n < 1){
+		return 0;
 	}
+	*size = size_n;
+	return 1;
 }
 
-float myFloatInput() {
-	int t;
-	float res;
-
-	t = scanf_s("%f", &res);
-
-	while (!t) {
-		printf("You entered not a number\n");
-		scanf_s("%*c");
-		t = scanf_s("%f", &res);
+int matrixCIN(float matrix[][N], int size_n, int size_m) {
+	for (int i = 0; i < size_n; i++) {
+		for (int j = 0; j < size_m; j++) {
+			if (!scanf_s("%f", &matrix[i][j])) {
+				return 0;
+			}
+		}
 	}
-
-	return res;
+	return 1;
 }
 
 void matrixCOUT(float matrix[][N], int size_n, int size_m) {
@@ -37,31 +31,55 @@ void matrixCOUT(float matrix[][N], int size_n, int size_m) {
 	}
 }
 
+float findMin(const float matrix[][N], int size_n, int size_m) {
+	float min = matrix[0][0];
+	for (int i = 0; i < size_n; i++) {
+		for (int j = 0; j < size_m; j++) {
+			float currEl = matrix[i][j];
+			if (min > currEl) min = currEl;
+		}
+	}
+	return min;
+}
+
+float findMax(const float matrix[][N], int size_n, int size_m) {
+	float max = matrix[0][0];
+	for (int i = 0; i < size_n; i++) {
+		for (int j = 0; j < size_m; j++) {
+			float currEl = matrix[i][j];
+			if (max < currEl) max = currEl;
+		}
+	}
+	return max;
+}
+
+void composeNewMatrixFromOld(const float matrixOld[][N], float matrixNew[][N], int size_n, int size_m) {
+	float diff = 0;
+	for (int i = 0; i < size_n; i++) {
+		for (int j = 0; j < size_m; j++) {
+			diff = findMax(matrixOld, i + 1, j + 1) - findMin(matrixOld, i + 1, j + 1);
+			matrixNew[i][j] = diff;
+		}
+	}
+}
+
 void main() {
 	int n = 0;
-	float maxA, minA, t, arrayA[N][N], arrayB[N][N];
+	float arrayA[N][N], arrayB[N][N];
 
 	printf("Input matrix size\n");
-	t = scanf_s("%d", &n);
-
-	if (!t || n > 20 || n < 1) {
+	if (!mSizeInput(&n)) {
 		printf("You entered invalid matrix size, input integer greater than 0 less than %d\n", N);
 		return;
 	}
 
 	printf("Input matrix\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			arrayA[i][j] = myFloatInput();
-		}
+	if (!matrixCIN(arrayA, n, n)) {
+		printf("You entered not a number\n");
+		return;
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			findMinMax(arrayA, i + 1, j + 1, &maxA, &minA);
-			arrayB[i][j] = maxA - minA;
-		}
-	}
+	composeNewMatrixFromOld(arrayA, arrayB, n, n);
 
 	printf("\nYour matrix\n");
 	matrixCOUT(arrayA, n, n);
